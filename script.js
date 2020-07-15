f18e6d7301ea9a9757581622de0ed2fc01753184
Abdirahman Hassan <ahgbered10@gmail.com>
2020-07-15 19:25:52 +0300

    Added npm package and also the lodash package. Also changed the main script to the browserify bundle.js

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
-var body = document.querySelector("body");
-var button = document.querySelector("button");
-var h3Element = document.querySelector("h3");
+let _ = require('lodash');
 
-var color1 = document.querySelector("#color1");
-var color2 = document.querySelector("#color2");
+// var array = [
+// 	1,
+// 	2,
+// 	3,
+// 	4,
+// 	5,
+// 	6,
+// 	7
+// ];
+// console.log('answer: ', _.without(array, 3));
 
-h3Element.textContent = "linear-gradient(to right, "+color1.value+" ,"+color2.value+")";
-color1.addEventListener("input", setGradient);
-color2.addEventListener("input", setGradient);
+var body = document.querySelector('body');
+var button = document.querySelector('button');
+var h3Element = document.querySelector('h3');
 
-function setGradient(){
-    body.style.background="linear-gradient(to right, "+color1.value+" ,"+color2.value+")";
-    h3CssGradient();
-};
+var color1 = document.querySelector('#color1');
+var color2 = document.querySelector('#color2');
 
-function h3CssGradient(){
-    h3Element.textContent = "linear-gradient(to right, "+color2.value+" ,"+color2.value+")";
-};
+h3Element.textContent = 'linear-gradient(to right, ' + color1.value + ' ,' + color2.value + ')';
+color1.addEventListener('input', setGradient);
+color2.addEventListener('input', setGradient);
+
+function setGradient() {
+	body.style.background = 'linear-gradient(to right, ' + color1.value + ' ,' + color2.value + ')';
+	h3CssGradient();
+}
+
+function h3CssGradient() {
+	h3Element.textContent = 'linear-gradient(to right, ' + color2.value + ' ,' + color2.value + ')';
+}
